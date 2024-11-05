99b2df4605df7749992b2032f8465db87b0963ed
arzelcm <arzelcanavate@gmail.com>
2024-11-05 13:34:16 +0100

    Fix norme errors

--- src/render/scene/settings/parser/scene_settings_parser.h
+++ src/render/scene/settings/parser/scene_settings_parser.h
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scene_settings_parser.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 13:32:33 by arcanava          #+#    #+#             */
+/*   Updated: 2024/11/05 13:32:41 by arcanava         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 #include "parser/parser.h"
 #include "../scene_settings.h"
 
 int	try_parse_scene_setting(t_parser_ctx *ctx, char **params,
-			t_scene_settings *settings);
+		t_scene_settings *settings);
