d9e94880df90760eb0009c7979438158151d9b08
cfareste <chrisfidare@yahoo.com>
2024-11-18 14:47:01 +0100

    Fix norme errors

--- src/scene/figure/material/types/emissive/emissive.h
+++ src/scene/figure/material/types/emissive/emissive.h
@@ -6,7 +6,7 @@
 /*   By: cfidalgo <cfidalgo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 18:34:02 by cfidalgo          #+#    #+#             */
-/*   Updated: 2024/11/18 12:48:55 by cfidalgo         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:46:51 by cfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,5 @@ typedef struct s_emissive_attrs
 
 typedef struct s_material	t_material;
 
-void	parse_emissive(t_parser_ctx *ctx, char *attrs_str, t_material *material);
+void	parse_emissive(t_parser_ctx *ctx, char *attrs_str,
+			t_material *material);
