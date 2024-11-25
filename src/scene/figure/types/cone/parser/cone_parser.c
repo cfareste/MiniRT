1f824226ca88270054c889f31c1dfeb17c01116f
cfareste <chrisfidare@yahoo.com>
2024-11-25 16:59:33 +0100

    Update cone and cylinder height parsing rage

--- src/scene/figure/types/cone/parser/cone_parser.c
+++ src/scene/figure/types/cone/parser/cone_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cone_parser.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
+/*   By: cfidalgo <cfidalgo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 16:22:22 by cfidalgo          #+#    #+#             */
-/*   Updated: 2024/11/19 23:43:35 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/11/25 16:54:41 by cfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void	check_cone_parsing(t_parser_ctx *ctx, t_figure *cone)
 	check_ori_vector_parsing(ctx, &cone->co_attrs->orientation);
 	if (cone->co_attrs->radius <= 0)
 		throw_parse_err(ctx, "Cone diameter must be a positive value");
-	else if (cone->co_attrs->height < 0)
+	else if (cone->co_attrs->height <= 0)
 		throw_parse_err(ctx, "Cone height must be a positive value");
 }
