554074837ed0491203ac8ccd0ece576fbfd3728f
cfareste <chrisfidare@yahoo.com>
2024-12-03 00:07:59 +0100

    Add group test abstraction refactor

--- tester/tests/scene_settings/scene_settings_test.sh
+++ tester/tests/scene_settings/scene_settings_test.sh
@@ -6,74 +6,10 @@ reset_scene_settings_test(){
 	IFS=$'\n'
 }
 
-check_test_result(){
-	local expected_exit_code=1
-
-	if [ "$2" = "SUCCESS" ]
-	then
-		expected_exit_code=130
-	fi
-
-	if [ $expected_exit_code -ne $3 ] || ([ "$4" != "$5" ] && [ ! -z "$5" ])
-	then
-		IFS=$'\n'
-		local parsed_actual="(No error returned)"
-		if [ ! -z "$5" ]
-		then
-			splitted_actual=($5)
-			parsed_actual=${splitted_actual[1]}
-		fi
-		printf "$RED_BOLD$WRONG_ICON$DEF_COLOR\n"
-		printf $WHITE_BOLD"Testing: $PINK\"$1\"\n"$DEF_COLOR
-		printf $WHITE_BOLD"Expected: $CYAN$4$DEF_COLOR\n"
-		printf $WHITE_BOLD"Actual: $RED${parsed_actual}$DEF_COLOR\n\n"
-		return 1
-	else
-		printf "$GREEN_BOLD$CORRECT_ICON$DEF_COLOR\n"
-	fi
-
-	return 0
-}
-
 test_scene_settings(){
-	local test_id=1
-	local output
-	local test_status
-	local global_result=0
-	reset_scene_settings_test
+	local group_test_result=0
 
-	for line in $(cat < $SCENE_SETTINGS_UC)
-	do
-		IFS=';'
-		local tokens=""
-		local raw_tokens=($line)
-		local expected_output="(No error expected)"
-
-		printf "Test $PINK_BOLD$test_id$DEF_COLOR: "
-
-		tokens[0]=${raw_tokens[0]}
-		for ((i = 1; i < ${#raw_tokens[@]}; i++))
-		do
-			tokens[$i]=$(echo "${raw_tokens[$i]}" | trim_string)
-		done
-
-		insert_element_in_scene "${tokens[0]}"
-		output=$(run_test)
-		test_status=$?
-		if [ ${#raw_tokens[@]} -eq 3 ]
-		then
-			expected_output="$TEST_SCENE -> ${tokens[2]}"
-		fi
-		check_test_result "${tokens[0]}" "${tokens[1]}" $test_status "$expected_output" "$output"
-		local test_result=$?
-		if [ $test_result -ne 0 ]
-		then
-			global_result=1
-		fi
-		((test_id++))
-		reset_scene_settings_test
-	done
-
-	IFS="$OLDIFS"
-	return $global_result
+	execute_group_test 'reset_scene_settings_test' $SCENE_SETTINGS_UC
+	group_test_result=$?
+	return $group_test_result
 }
--- tester/utils/assert/assert.sh
+++ tester/utils/assert/assert.sh
@@ -16,3 +16,75 @@ run_test(){
 	echo "$output"
 	return $exit_status
 }
+
+process_test_result(){
+	local expected_exit_code=1
+
+	if [ "$2" = "SUCCESS" ]
+	then
+		expected_exit_code=130
+	fi
+
+	if [ $expected_exit_code -ne $3 ] || ([ "$4" != "$5" ] && [ ! -z "$5" ])
+	then
+		IFS=$'\n'
+		local parsed_actual="(No error returned)"
+		if [ ! -z "$5" ]
+		then
+			splitted_actual=($5)
+			parsed_actual=${splitted_actual[1]}
+		fi
+		printf "$RED_BOLD$WRONG_ICON$DEF_COLOR\n"
+		printf $WHITE_BOLD"Testing: $PINK\"$1\"\n"$DEF_COLOR
+		printf $WHITE_BOLD"Expected: $CYAN$4$DEF_COLOR\n"
+		printf $WHITE_BOLD"Actual: $RED${parsed_actual}$DEF_COLOR\n\n"
+		return 1
+	else
+		printf "$GREEN_BOLD$CORRECT_ICON$DEF_COLOR\n"
+	fi
+
+	return 0
+}
+
+execute_group_test(){
+	local test_id=1
+	local output
+	local test_status
+	local group_result=0
+
+	$1
+	for line in $(cat < "$2")
+	do
+		IFS=';'
+		local tokens=""
+		local raw_tokens=($line)
+		local expected_output="(No error expected)"
+
+		printf "Test $PINK_BOLD$test_id$DEF_COLOR: "
+
+		tokens[0]=${raw_tokens[0]}
+		for ((i = 1; i < ${#raw_tokens[@]}; i++))
+		do
+			tokens[$i]=$(echo "${raw_tokens[$i]}" | trim_string)
+		done
+
+		insert_element_in_scene "${tokens[0]}"
+		output=$(run_test)
+		test_status=$?
+		if [ ${#raw_tokens[@]} -eq 3 ]
+		then
+			expected_output="$TEST_SCENE -> ${tokens[2]}"
+		fi
+		process_test_result "${tokens[0]}" "${tokens[1]}" $test_status "$expected_output" "$output"
+		local test_result=$?
+		if [ $test_result -ne 0 ]
+		then
+			group_result=1
+		fi
+		((test_id++))
+		$1
+	done
+
+	IFS="$OLDIFS"
+	return $group_result
+}
