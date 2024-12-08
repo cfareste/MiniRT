d159ac340ad57a9d2193ddcfe5520d6e5371ad24
cfareste <chrisfidare@yahoo.com>
2024-12-08 19:33:17 +0100

    Add bright color to selected object in normal map mode

--- src/render/strategies/normal_map/normal_map.c
+++ src/render/strategies/normal_map/normal_map.c
@@ -6,12 +6,13 @@
 /*   By: cfidalgo <cfidalgo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:30:38 by cfidalgo          #+#    #+#             */
-/*   Updated: 2024/11/28 18:33:48 by cfidalgo         ###   ########.fr       */
+/*   Updated: 2024/12/08 17:18:40 by cfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scene/scene.h"
 #include "render/strategies/shared/strategies_shared.h"
+#include "render/utils/color/color_operations/color_operations.h"
 #include "libft.h"
 
 void	compute_normal_map(t_scene *scene, t_ray *ray, t_color *sample_color)
@@ -21,4 +22,6 @@ void	compute_normal_map(t_scene *scene, t_ray *ray, t_color *sample_color)
 	ft_bzero(&hit_record, sizeof(t_hit_record));
 	check_collisions(scene, ray, &hit_record);
 	get_normal_color(sample_color, &hit_record.normal);
+	if (scene->selection && scene->selection == hit_record.figure)
+		sum_color_scalar(sample_color, 0.3, sample_color);
 }
--- src/render/utils/color/color_operations/color_operations.h
+++ src/render/utils/color/color_operations/color_operations.h
@@ -6,11 +6,13 @@
 /*   By: cfidalgo <cfidalgo@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:07:24 by cfidalgo          #+#    #+#             */
-/*   Updated: 2024/11/19 11:40:41 by cfidalgo         ###   ########.fr       */
+/*   Updated: 2024/12/08 17:17:33 by cfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render/utils/color/color.h"
+
+void	sum_color_scalar(t_color *color, float scalar, t_color *res);
 
 void	sum_colors(t_color *first_color, t_color *second_color, t_color *res);
 
